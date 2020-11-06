271801f8b03cb5f038821472c7eb73d0288d99ed
Bradley Pearson <bradley.pearson6597@gmail.com>
2020-11-06 15:52:05 +0000

    Layout of app created.

--- ui.R
+++ ui.R
@@ -3,5 +3,27 @@
 #######################################################################################################
 
 ui <- semanticPage(
+  title = "Marine App",
+  tags$head(tags$style(".leaflet-control-attribution {
+                                      display:none;
+                       }"
+  )),
   
+  flow_layout(cell_width = "40%",
+              leafletOutput("vesselmap"),
+              vertical_layout(
+                uiOutput("vesseldist"),
+                uiOutput("vesseltime"),
+                uiOutput("vesselspeed"),
+                uiOutput("vesseldestination")
+                )
+              ),
+  flow_layout( 
+    p("Choose Vessel Type:"),
+    p("Choose Vessel Name:")
+  ),
+  flow_layout(
+    dropdown_input("vessel_type", unique(ships.data$ship_type), type = "selection multiple", value = ""),
+    uiOutput("vesselname")
+  )
 )
